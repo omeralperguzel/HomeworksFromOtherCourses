08c59c31b267c75fa0160c4d4a782e680c55f233
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-08-18 01:22:46 +0300

    kodluyoruz coderbyte week 2 test 1 added

--- PatikaJava101/Practice/NotOrtalamasiHesaplayanProgram.java
+++ PatikaJava101/Practice/NotOrtalamasiHesaplayanProgram.java
@@ -1,3 +1,9 @@
+/*
+QUESTION:
+Have the function SnakeCase(str) take the str parameter being passed and return it in proper snake case format where each word is lowercased and separated from adjacent words via an underscore. The string will only contain letters and some combination of delimiter punctuation characters separating each word.
+
+For example: if str is "BOB loves-coding" then your program should return the string bob_loves_coding.*/
+
 package PatikaJava101.Practice;
 
 import java.util.Scanner;
