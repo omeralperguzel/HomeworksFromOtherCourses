681e4c234f0b598f9a6a1498889df5940e2026c5
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-08-18 17:04:20 +0300

    Create KDVTutariHesaplayanProgram.java
    new practice program added to the repo

---
+++ PatikaJava101/Practice/KDVTutariHesaplayanProgram.java
@@ -0,0 +1,22 @@
+package PatikaJava101.Practice;
+
+import java.util.Scanner;
+
+public class KDVTutariHesaplayanProgram {
+  public static void main(String[] args){
+    double kdvsizfiyat;
+    //kdvsiz tutarın elde edilmesi
+    Scanner girdi = new Scanner(System.in);
+    System.out.println(“Ücret Tutarını Giriniz: ”);
+    kdvsiztutar = girdi.nextDouble();
+    //hesaplama
+    double kdv = 18/100;
+    double kdvtutari = kdvsizfiyat*kdv;
+    double kdvlifiyat = kdvsizfiyat*(1+kdv);
+    //yazdırma
+    System.out.println(“KDV Hariç Tutar: ” + kdvsiztutar);
+    System.out.println(“KDV Oranı: ” + kdv);
+    System.out.println(“KDV Tutarı: ” + kdvtutari);
+    System.out.println(“KDV Dahil Fiyat: ” + kdvlifiyat);
+  }
+}
