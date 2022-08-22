391c0dc35ef1b43f2f7a821c515083015e991caa
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-08-22 03:55:26 +0300

    python commits

---
+++ Hackerrank/PythonBadge/RunnerUpScoreFinder.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    secondnum = float('-inf') ;topnum = float('-inf')
+    for i, n in enumerate(arr):
+
+      if n > topnum:
+        secondnum = topnum
+        topnum = n
+      elif n < topnum and n > secondnum:
+        secondnum = n
+
+    print(secondnum)  
