15db3f0c1f77824007fe6044ff9288cdfa7c2850
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-06 00:56:35 +0300

    add one java patika practice (week 4 pr 7)

--- PatikaJava101/Practice/HarmonikSayilariBulanProgram.java
+++ PatikaJava101/Practice/HarmonikSayilariBulanProgram.java
@@ -1,5 +1,21 @@
 package PatikaJava101.Practice;
 
+import java.util.Scanner;
+
 public class HarmonikSayilariBulanProgram {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını giriniz: ");
+        int n = input.nextInt();
+        double result = 0.0;
+        for (double i = 1; i <= n; i++){
+            result += (1/i);
+        }
+
+        System.out.println(result);
+
+        input.close();
+    }
     
 }
