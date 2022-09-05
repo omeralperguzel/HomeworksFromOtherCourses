8761689dd4d82239c37f7d3e3059af77fb588e9e
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-06 02:18:53 +0300

    add 1 patika java prac (week 4 prac 8)

---
+++ PatikaJava101/Practice/YildizlarIleUcgenYapimi.java
@@ -0,0 +1,34 @@
+package PatikaJava101.Practice;
+
+import java.util.Scanner;
+
+public class YildizlarIleUcgenYapimi {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = keyboard.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }  
+        for (int i = (n-1); i >= 0 ; i--) {
+            for (int j = (n - i); j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i); k >= 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }   
+
+        keyboard.close();
+    }
+    
+}
