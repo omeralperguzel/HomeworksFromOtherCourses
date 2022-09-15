54056166c852997fc33d22766ca824c83085fce6
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-15 17:24:34 +0300

    .

--- CoderbyteExamsFromKodluyoruz/StajProgrami/Week6/MeanMode1.java
+++ CoderbyteExamsFromKodluyoruz/StajProgrami/Week6/MeanMode1.java
@@ -1,5 +1,5 @@
 /*QUESTION: (EASY)
- 
+Have the function MeanMode(arr) take the array of numbers stored in arr and return 1 if the mode equals the mean, 0 if they don't equal each other (ie. [5, 3, 3, 3, 1] should return 1 because the mode (3) equals the mean (3)). The array will not be empty, will only contain positive integers, and will not contain more than one mode. 
  */
 
 //https://stackoverflow.com/questions/4191687/how-to-calculate-mean-median-mode-and-range-from-a-set-of-numbers
