0cd1065b195e93f72f30face1ac125a8ba04f79a
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-15 17:19:10 +0300

    fix one coderbyte challenge

--- CoderbyteExamsFromKodluyoruz/StajProgrami/Week6/WildcardCharacters1.java
+++ CoderbyteExamsFromKodluyoruz/StajProgrami/Week6/WildcardCharacters1.java
@@ -10,13 +10,64 @@ For example: if str is "++*{5} gheeeee" then the second string in this case does
 package CoderbyteExamsFromKodluyoruz.StajProgrami.Week6;
 
 import java.util.*; 
-import java.io.*;
+//import java.io.*;
 
 public class WildcardCharacters1 {
 
     public static String WildcardCharacters(String str) {
         // code goes here  
-        return str;
+        String[] splitinput = str.split(" ");
+        String s1 = splitinput[0];
+        String s2 = splitinput[1];
+    
+        int t1 = 0;
+        int t2 = 0;
+        char charAtT1;
+        char charAtT2;
+    
+        while (t1 < s1.length() && t2 < s2.length()){
+          charAtT1 = s1.charAt(t1);
+          charAtT2 = s2.charAt(t2);
+          if (charAtT1 == '+'){
+            if (charAtT2 >= 91 && charAtT2 <= 122){
+              t1++;
+              t2++;
+            }
+            else {
+              return "false";
+            }
+          }
+          else if (charAtT1 == '*'){
+            Character previousCharacter = null;
+            int loopUpto;
+            if (t1 + 1 == s1.length()){
+              loopUpto = 3;
+              t1 = t1 + 1;
+            }
+            else if (s1.charAt(t1 + 1) == '{'){
+              loopUpto = s1.charAt(t1+2) - '0';
+              t1 = t1 + 4;
+            }
+            else {
+              loopUpto = 3;
+              t1 = t1 + 1;
+            }
+    
+            if (t2 + loopUpto > s2.length()) return "false";
+            while (loopUpto-- > 0){
+              if (previousCharacter == null){
+                previousCharacter = s2.charAt(++t2);
+              }
+              else{
+                char charAtNewT2 = s2.charAt(t2);
+                if (previousCharacter != charAtNewT2) return "false";
+                t2++;
+              }
+            }
+          }
+        }
+        if (t1 == s1.length() && t2 == s2.length()) return "true";
+        return "false";
       }
     
       public static void main (String[] args) {  
@@ -27,3 +78,9 @@ public class WildcardCharacters1 {
       }
     
 }
+
+//RESULT: 12/15
+/*
+1. For input "*{1}*{1} rt" the output was incorrect. The correct output is true
+2. For input "+*{10}+ gphpppppppppp" the output was incorrect. The correct output is false 
+*/
