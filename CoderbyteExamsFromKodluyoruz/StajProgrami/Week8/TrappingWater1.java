6ba8bf33eef67ea79a84599b6afb75bbc037d1eb
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-29 17:27:06 +0300

    Update TrappingWater1.java

--- CoderbyteExamsFromKodluyoruz/StajProgrami/Week8/TrappingWater1.java
+++ CoderbyteExamsFromKodluyoruz/StajProgrami/Week8/TrappingWater1.java
@@ -1,3 +1,15 @@
+/* QUESTION: (MEDIUM)
+Have the function TrappingWater(arr) take the array of non-negative integers stored in arr, and determine the largest amount of water that can be trapped. The numbers in the array represent the height of a building (where the width of each building is 1) and if you imagine it raining, water will be trapped between the two tallest buildings. For example: if arr is [3, 0, 0, 2, 0, 4] then this array of building heights looks like the following picture if we draw it out: 
+
+(Object needed) 
+
+Now if you imagine it rains and water gets trapped in this picture, then it'll look like the following (the x's represent water): 
+
+(Object needed)
+
+This is the most water that can be trapped in this picture, and if you calculate the area you get 10, so your program should return 10.
+
+*/
 import java.util.*; 
 import java.io.*;
 
