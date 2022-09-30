6d4c160b355865919a07fb8d5e7ef2d1c82dc63a
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2022-09-30 21:40:34 +0300

    add java patika prac (week 8 prac 4)

---
+++ PatikaJava101/Practice/IkiBoyutluDizilerleBHarfiYazma1.java
@@ -0,0 +1,42 @@
+package PatikaJava101.Practice;
+
+public class IkiBoyutluDizilerleBHarfiYazma1 {
+
+    public static void main(String[] args) {
+        
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 6) {
+                    letter[i][j] = " * ";
+                } 
+                else if (j == 0 || j == 3) {
+                    if (i != 3){
+                        letter[i][j] = " * ";
+                    }
+                    else{
+                        letter[i][j] = "   ";
+                    }
+                } 
+                else {
+                    letter[i][j] = "   ";
+                }
+            }
+            for (int j = 0; j < letter[i].length-1; j++) {
+                if (i == 3) {
+                    letter[i][j] = " * ";
+                }
+            }
+ 
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+    
+}
